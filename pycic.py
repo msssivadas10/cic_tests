b08e8cce2fb71d53daab91cd0f4a6cd9d21e4e7e
M. S. Suryan Sivadas <ms3.msssivadas@gmail.com>
2022-01-24 15:42:34 +0530

    Updated code

--- pycic.py
+++ pycic.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
-from typing import Any
 import numpy as np
+from typing import Any
+from scipy.interpolate import CubicSpline
 
 class CICError(Exception):
     """
     Error raised by functions and classes related to CIC computations.
     """
     ...
-
 
 def cart2redshift(x: Any, v: Any, z: float, los: Any = ...) -> Any:
     r"""
@@ -91,7 +91,53 @@ def cart2redshift(x: Any, v: Any, z: float, los: Any = ...) -> Any:
 
     return s
 
+class cicDistribution:
+    r"""
+    Implementation of the theoretical count-in-cells distribution given in Repp
+    and Szapudi (2020). 
 
+    Parameters
+    ----------
+    pk_table: array_like
+        Power spectrum table. This must be a 2D array with two columns - :math:`\ln k`
+        in the first and :math:`\ln P(k)` in the second. The table should have enough 
+        resolution to include the details, if present.
+    z: float
+        Redshift value. Must be greater than -1. 
+    Om0: float
+        Present value of the normalized matter density, :math:`\Omega_{\rm m}`.
+    Ode0: float
+        Present value of the normalized dark-energy density, :math:`\Omega_{\rm de}`.
+    h: float
+        Presnt value of the Hubble parameter in units of 100 km/sec/Mpc.
+
+    """
+    __slots__ = "pk_spline", "z", "Om0", "Ode0", "h", 
+
+    def __init__(self, pk_table: Any, z: float, Om0: float, Ode0: float, h: float) -> None:
+        if z < -1.:
+            raise CICError("redshift cannot be less than -1")
+        self.z = z
+
+        if Om0 < 0.:
+            raise CICError("Om0 cannot be negative")
+        self.Om0 = Om0
+
+        if Ode0 < 0.: 
+            raise CICError("Ode0 cannot be negative")
+        self.Ode0 = Ode0
+
+        if h < 0.:
+            raise CICError("h cannot be neative")
+        self.h = h
+
+        pk_table = np.asarray(pk_table)
+        if pk_table.ndim != 2:
+            raise CICError("power spectrum should be given as a table")
+        elif pk_table.shape[1] != 2:
+            raise CICError("power table should have two columns")
+        lnk, lnpk      = pk_table.T
+        self.pk_spline = CubicSpline(lnk, lnpk)
 
 
 
