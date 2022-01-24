16cf86b545ee4c5e2ac9e1a7a9d615efe6bec866
M. S. Suryan Sivadas <ms3.msssivadas@gmail.com>
2022-01-24 13:11:20 +0530

    Updated code

--- pycic.py
+++ pycic.py
@@ -10,7 +10,7 @@ class CICError(Exception):
     ...
 
 
-def cart2redshift(x: Any, v: Any, z: float, los: Any = ..., Om0: float = 0.3, Ode0: float = 0.7, H0: float = 70.0):
+def cart2redshift(x: Any, v: Any, z: float, los: Any = ...) -> Any:
     r"""
     Convert a real (cartetian) space galaxy catalog to a redshift space catalog 
     using the plane parallel transformation.
@@ -19,20 +19,46 @@ def cart2redshift(x: Any, v: Any, z: float, los: Any = ..., Om0: float = 0.3, Od
         {\bf s} = {\bf x} + ({\bf v} \cdot \hat{\bf l}) \frac{(z + 1)\hat{\bf l}}{H}
 
     where, :math:`{\bf s}` and :math:`{\bf x}` are the comoving position of the 
-    galaxies in redshift and real spaces, respectively. :math:`{\bf l}` is the line
-    of sight vector.
+    galaxies in redshift and real spaces, respectively. :math:`{\bf l}` is the 
+    line-of-sight vector.
 
     Parameters
     ----------
+    x: array_like
+        Comoving position coordinates of the galaxies. Must be an array with 3 
+        columns (corrsponding to 3 cartesian coordinates of the position).
+    v: array_like
+        Peculiar velocities of the galaxies in units of the Hubble parameter 
+        (i.e., now both the distance and velocity will be in same units). It should 
+        have the same shape as the position array.
+    z: float
+        Redshift corresponding to the present configuration.
+    los: array_like, str
+        Line-of-sight vector. Position corrdinates are transformed along this direction.
+        It can be a 3-vector or any of `x`, `y` or `z` (directions). If not specified, 
+        use the radial direction.
 
     Returns
     -------
+    s: array_like
+        Comoving position of galaxies in redshift space. This will have the same shape 
+        as the input position coordinates.
+
+    Examples
+    --------
+    Let us create a random position catalog catalog in a 500 unit box, with randomized 
+    velocity in the range [-10, 10]. Then the transformed position with z-direction as
+    the line-of will be 
+
+    >>> x = np.random.uniform(0., 500., (8, 3))
+    >>> v = np.random.uniform(-10., 10., (8, 3))
+    >>> s = pycic.cart2redshift(x, v, 0., 'z')
 
     """
-    # get the hubble parameter:
-    zp1 = 1 + z
-    Hz  = H0 * np.sqrt(Om0 * zp1**3 + (1. - Om0 - Ode0) * zp1**2 + Ode0)
+    if z < -1: # check redshift
+        raise CICError("redshift must not be less than -1")
 
+    # check input data:
     x, v = np.asarray(x), np.asarray(v)
     if x.ndim != 2 or v.ndim != 2:
         raise CICError("position and velocity should be a 2D array")
@@ -45,12 +71,11 @@ def cart2redshift(x: Any, v: Any, z: float, los: Any = ..., Om0: float = 0.3, Od
     if los is ... :
         los = x
     elif isinstance(los, str):
-        if los == 'x':
-            los = np.array([1., 0., 0.])
-        elif los == 'y':
-            los = np.array([0., 1., 0.])
-        elif los == 'z':
-            los = np.array([0., 0., 1.])
+        if len(los) == 1 and los in 'xyz':
+            los = np.where(
+                            np.array(['x', 'y', 'z']) == los,
+                            1., 0.
+                          )
         else:
             raise CICError(f"invalid key for los, `{los}`")
     else:
@@ -59,3 +84,17 @@ def cart2redshift(x: Any, v: Any, z: float, los: Any = ..., Om0: float = 0.3, Od
             raise CICError("los must be 1D array (vector)")
         elif los.shape[0] != 3:
             raise CICError("los must be a 3-vector")
+    los = los / np.sum(los**2, axis = -1) 
+
+    # plane parallel transformation:
+    s = x + los * np.sum(v * los, axis = -1)[:, np.newaxis] * (1. + z)
+
+    return s
+
+
+
+
+
+
+
+
--- testing.py
+++ testing.py
@@ -3,6 +3,15 @@
 import numpy as np
 import pycic
 
-x = np.random.uniform(0., 500., (256, 3))
-v = np.random.uniform(-10., 10., (256, 3))
+x = np.random.uniform(0., 500., (512, 3))
+v = np.random.uniform(0., 10.,  (512, 3))
 
+s = pycic.cart2redshift(x, v, 0., 'z')
+
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
+
+fig, (ax1, ax2) = plt.subplots(1, 2)
+ax1.plot(x[:,0], x[:,2], 'o', ms = 2.)
+ax2.plot(s[:,0], s[:,2], 'o', ms = 2.)
+plt.show()
