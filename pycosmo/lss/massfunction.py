fedb9f1163a3634cab4f5fe8a70eb87006dc4964
M. S. Suryan Sivadas <ms3.msssivadas@gmail.com>
2022-04-23 02:55:46 +0530

    Update massfunction.py

--- pycosmo/lss/massfunction.py
+++ pycosmo/lss/massfunction.py
@@ -1,12 +1,17 @@
 #!\usr\bin\python3
 
-from typing import Any, Callable, List
+from typing import Any
+from pycosmo.utils import FunctionTable, addfunction
 from pycosmo.cosmology import Cosmology
-from scipy.interpolate import CubicSpline
-import numpy as np
+from pycosmo.lss.helpers import mdefParser
 import pycosmo.constants as const
+import numpy as np
 
-def press74(sigma: Any):
+
+ftable = FunctionTable( 'LinearBias' )
+
+@addfunction( 'press74', ftable, fclass = 'bias', mdef = [ 'fof' ] )
+def mf_Press74(sigma: Any):
     r"""
     Fitting function by Press and Schechter (1974). 
     """
@@ -14,7 +19,8 @@ def press74(sigma: Any):
     f  = np.sqrt( 2 / np.pi ) * nu * np.exp( -0.5 * nu**2 )
     return f
 
-def sheth01(sigma: Any):
+@addfunction( 'sheth01', ftable, fclass = 'bias', mdef = [ 'fof' ] )
+def mf_Sheth01(sigma: Any):
     r"""
     Fitting function by Sheth et. al. in 2001.
     """
@@ -22,10 +28,11 @@ def sheth01(sigma: Any):
     a = 0.707
     p = 0.3
     nu = const.DELTA_C / np.asarray( sigma )
-    f = A * np.sqrt( 2*a / np.pi ) * nu * np.exp( -0.5 * a * nu**2 ) * ( 1.0 + ( 1.0/a/nu**2 )**p )
+    f = A * np.sqrt( 2*a / np.pi ) * nu * np.exp( -0.5 * a * nu**2 ) * ( 1.0 + ( nu**2 / a )**-p )
     return f
 
-def tinker08(sigma: Any, z : float, Delta: float):
+@addfunction( 'tinker08', ftable, fclass = 'bias', z = True, mdef = [ 'so' ] )
+def mf_Tinker08(sigma: Any, z : float, Delta: float):
     r""" 
     Fitting function by J. Tinker et. al. in 2008[1]. 
     """
@@ -40,6 +47,8 @@ def tinker08(sigma: Any, z : float, Delta: float):
         raise ValueError('`Delta` value is out of bound. must be within 200 and 3200.')
 
     # find interpolated values from 0-redshift parameter table : table 2
+    from scipy.interpolate import CubicSpline
+
     A  = CubicSpline(
                         [200,   300,   400,   600,   800,   1200,  1600,  2400,  3200 ],
                         [0.186, 0.200, 0.212, 0.218, 0.248, 0.255, 0.260, 0.260, 0.260],
@@ -68,170 +77,134 @@ def tinker08(sigma: Any, z : float, Delta: float):
     f = A * ( 1 + ( b / sigma )**a ) * np.exp( -c / sigma**2 )
     return f
 
-
-def mdefParser(value: str, z: float = None, cm: Cosmology = None) -> tuple:
+@addfunction( 'jenkins01', ftable, fclass = 'bias', mdef = [ 'fof' ] )
+def mf_Jenkins01(sigma: Any) -> Any:
     """
-    Parse a mass definition string.
+    Fitting function by Jenkins et al. (2001).
     """
-    import re
+    sigma = np.asfarray( sigma )
+    return 0.315*( -np.abs( np.log( sigma**-1 ) + 0.61 )**3.8 )
 
-    m = re.match( r'(\d*)([mc]|[fovir]{3})', value )
-    if m is None:
-        raise ValueError("cannot parse mass definition: '{}'".format( value ))
+addfunction( 'reed03', ftable, fclass = 'bias', mdef = [ 'fof' ] )
+def mf_Reed03(sigma: Any) -> Any:
+    """
+    Fitting function by Reed et al. (2003).
+    """
+    sigma = np.asfarray( sigma )
+    return mf_Sheth01( sigma ) * np.exp( -0.7 / ( sigma * np.cosh( 2*sigma )**5 ) )
 
-    delta, ref = m.groups()
-    if ref == 'fof':
-        return ( None, 'fof' )
+@addfunction( 'reed07', ftable, fclass = 'bias', z = True, cosmo = True, mdef = [ 'fof' ] )
+def mf_Reed07(sigma: Any, z: float, cm: Cosmology) -> Any:
+    """
+    Fitting function by Reed et al. (2007).
+    """
+    A, c, ca, p = 0.310, 1.08, 0.764, 0.3
 
-    if ref == 'vir':
-        x = cm.Omz( z ) - 1.0
-        if cm.flat:
-            delta_c = 18.0*np.pi**2 + 82.0*x - 39.0*x**2
-        elif cm.Ode0 == 0.0:
-            delta_c = 18.0*np.pi**2 + 60.0*x - 32.0*x**2
-        else:
-            raise ValueError("cannot use 'vir' mass definition")
-        return ( round( delta_c ) * cm.criticalDensity( z ), 'so' )
+    sigma = np.asfarray( sigma )
+    omega = np.sqrt( ca ) * const.DELTA_C / sigma
+
+    G1    = np.exp( -0.5*( np.log( omega ) - 0.788 )**2 / 0.6**2 )
+    G2    = np.exp( -0.5*( np.log( omega ) - 1.138 )**2 / 0.2**2 )
+
+    r     = cm.radius( sigma, z )
+    neff  = -6.0*cm.dlnsdlnm( r, 0.0 ) - 3.0
     
-    if delta:
-        if ref == 'm':
-            return ( int( delta ) * cm.Omz( z ), 'so' )
-        elif ref == 'c':
-            return ( int( delta ) * cm.criticalDensity( z ), 'so' )
+    # eqn. 12
+    bias = (
+                A * omega * np.sqrt( 2.0/np.pi ) 
+                    * np.exp( -0.5*omega - 0.0325*omega**p / ( neff + 3 )**2 )
+                    * ( 1.0 + 1.02*omega**( 2*p ) + 0.6*G1 + 0.4*G2 )
+           )
+    return bias
 
-    raise ValueError("incorrect mass definition: '{}'".format( value ))
-
-class MassFunction:
+@addfunction( 'warren06', ftable, fclass = 'bias', mdef = [ 'fof' ] )
+def mf_Warren06(sigma: Any) -> Any:
     """
-    Represents a mass function model.
+    Fitting function be Warren et al. (2006).
     """
-    __slots__ = '_f', 'allowed_mdef', 'z_dependent', 'cosmology_dependent', 'attrs', 
-    
-    def __init__(self, f: Callable, zdep: bool = False, mdef: List[str] = [ 'fof' ], cdep: bool = False, **attrs) -> None:
-        if not callable( f ):
-            raise TypeError("'f' must be a callable")
-        self._f    = f
+    A, a, b, c = 0.7234, 1.625, 0.2538, 1.1982
+    sigma = np.asfarray( sigma )
+    return A * ( sigma**-a + b ) * np.exp( -c / sigma**2 )
 
-        self.z_dependent         = bool( zdep )
-        self.cosmology_dependent = bool( cdep )
-        self.allowed_mdef        = mdef
-        self.attrs               = attrs
-        
-    def f(self, sigma: Any, cm: Cosmology = None, z: float = 0.0, mdef: str = 'fof') -> Any:
-        """
-        Calculate the fitting function for mass function.
-        """
-        sigma = np.asfarray( sigma )
+@addfunction( 'crocce10', ftable, fclass = 'bias', z = True, mdef = [ 'fof' ] )
+def mf_Crocce10(sigma: Any, z: float) -> Any:
+    """
+    Fitting function be Crocce et al. (2010).
+    """
+    Az = 0.580*( z + 1 )**-0.130
+    az = 1.370*( z + 1 )**-0.150
+    bz = 0.300*( z + 1 )**-0.084
+    cz = 1.036*( z + 1 )**-0.024
+    return Az * ( sigma**-az + bz ) * np.exp( -cz / sigma**2 )
 
-        if not ( isinstance( cm, Cosmology ) or cm is None ):
-            raise TypeError("'cm' must be a 'Cosmology'")
+@addfunction( 'courtin10', ftable, fclass = 'bias', mdef = [ 'fof' ] )
+def mf_Courtin10(sigma: Any) -> Any:
+    """
+    Fitting function be Courtin et al. (2010).
+    """
+    A  = 0.348
+    a  = 0.695
+    p  = 0.1
+    nu = const.DELTA_C / np.asarray( sigma )
+    f  = A * np.sqrt( 2*a / np.pi ) * nu * np.exp( -0.5 * a * nu**2 ) * ( 1.0 + ( nu**2 / a )**-p )
+    return f
 
-        args = { 'sigma': sigma }
 
-        if self.z_dependent:
-            if z < -1.0:
-                raise ValueError("redshift 'z' must be greater than -1")
-            args[ 'z' ] = z
+#########################################################################################################
 
-        delta, mdef = mdefParser( mdef, z, cm )
-        if mdef not in self.allowed_mdef:
-            raise ValueError("function is not defined for '{}' mass definitions".format( mdef ))
-
-        if mdef != 'fof':
-            args[ 'Delta' ] = round( delta / cm.rho_m( z ) )
-
-        if self.cosmology_dependent:
-            args[ 'cm' ] = cm
-
-        return self._f( **args )
-
-    def fr(self, r: Any, cm: Cosmology, z: float = 0.0, mdef: str = 'fof', **kwargs) -> Any:
-        """
-        Calculate the fitting function for mass function.
-        """
-        if not isinstance( cm, Cosmology ):
-            raise TypeError("'cm' must be a 'Cosmology'")
-        return self.f( np.sqrt( cm.variance( r, z, **kwargs ) ), cm, z, mdef )
-
-    def fm(self, m: Any, cm: Cosmology = None, z: float = 0.0, mdef: str = 'fof', **kwargs) -> Any:
-        """
-        Calculate the fitting function for mass function.
-        """
-        return self.fr( cm.lagrangianR( m ), cm, z, mdef, **kwargs )
-
-    def dndlnm(self, m: Any, cm: Cosmology, z: float = 0.0, mdef: str = 'fof', **kwargs) -> Any:
-        """
-        Calculate the halo mass-function.
-        """
-        m = np.asfarray( m )
-        r = cm.lagrangianR( m )
-        
-        return (
-                    self.fr( r, cm, z, mdef, **kwargs )
-                        * ( cm.rho_m( z ) / m )
-                        * ( -cm.dlnsdlnm( r, z, **kwargs ) )
-               )
-
-    def dndm(self, m: Any, cm: Cosmology, z: float = 0.0, mdef: str = 'fof', **kwargs) -> Any:
-        """
-        Calculate the halo mass-function.
-        """
-        return self.dndlnm( m, cm, z, mdef ) / np.asfarray( m )
-
-    def __call__(self, m: Any, cm: Cosmology, z: float = 0.0, mdef: str = 'fof', mode: str = 'dndlnm', **kwargs) -> Any:
-        """
-        Calculate the halo mass-function.
-        """
-        if mode == 'f':
-            return self.fm( m, cm, z, mdef, **kwargs )
-        elif mode == 'dndlnm':
-            return self.dndlnm( m, cm, z, mdef, **kwargs )
-        elif mode == 'dndm':
-            return self.dndm( m, cm, z, mdef, **kwargs )
-        raise ValueError("invalid value for 'mode': '{}'".format( mode ))
-
-_functab = {
-                'press74' : MassFunction( 
-                                            press74, 
-                                            zdep = False, 
-                                            mdef = [ 'fof' ], 
-                                            cdep = False 
-                                        ),
-                'sheth01' : MassFunction( 
-                                            sheth01, 
-                                            zdep = False, 
-                                            mdef = [ 'fof' ], 
-                                            cdep = False 
-                                        ),
-                'tinker08': MassFunction( 
-                                            tinker08, 
-                                            zdep = True, 
-                                            mdef = [ 'so' ], 
-                                            cdep = False 
-                                        ),
-           }
 
 def available(model: str) -> bool:
     """
-    Check if a mass-function model is available.
+    Check if the model is available.
     """
-    return ( model in _functab.keys() )
+    return ftable.exists( model )
 
-def massFunction(m: Any, cm: Cosmology, z: float = 0.0, mdef: str = 'fof', model: str = 'tinker08', mode: str = 'dndlnm', **kwargs) -> Any:
+def massFunction(m: Any, cm: Cosmology, z: float = 0.0, model: str = 'tinker08', mdef: str = 'fof', out: str = 'dndlnm') -> Any:
     """
-    Compute the halo mass-function of given model.
+    Compute the halo mass-function.
     """
-    if not available( model ):
-        raise ValueError("model not available: '{}'".format( model ))
-    mf = _functab[ model ]
-    return mf( m, cm, z, mdef, mode, **kwargs )
-        
+    if not isinstance( cm, Cosmology ):
+        raise TypeError("'cm' must be a 'Cosmology' object")
+
+    if z < -1.0:
+        raise ValueError("redshift 'z' must be greater than -1")
+
+    m = np.asfarray( m )
+    r = cm.lagrangianR( m ) # lagrangian radius
+
+    sigma = np.sqrt( cm.variance( r, z ) ) # sigma( m )
+
+    # compute f( sigma ) of given model:
+    if not ftable.exists( model ):
+        raise ValueError("model is not available: '{}'".format( model ))
+    f = ftable[ model ]
+
+    args = { 'sigma': sigma }
+
+    if f.z_dependent:
+        args[ 'z' ] = z
+
+    delta, mdef = mdefParser( mdef, z, cm )
+    if mdef not in f.mdef:
+        raise ValueError("invalid mass definition: '{}'".format( mdef ))
+    if mdef != 'fof':
+        args[ 'Delta' ] = round( delta / cm.rho_m( z ) )
+
+    if f.cosmo_dependent:
+        args[ 'cm' ] = cm
+
+    fsigma = f( **args )
+    if out == 'f':
+        return fsigma
+
+    # compute dn( m )/dln( m ) and dn( m )/dm:
+    dndlnm = fsigma * ( cm.rho_m( z ) / m ) * ( -cm.dlnsdlnm( r, z ) )
+    if out == 'dndlnm':
+        return dndlnm
+    elif out == 'dndm':
+        return dndlnm / m
+
+    raise ValueError("invalid value for out: '{}'".format( out ))
 
 
-        
-
-        
-
-
-    
-
+     
