9ef201267d2873f924ca678da70bd03975dd7dfa
M. S. Suryan Sivadas <ms3.msssivadas@gmail.com>
2022-05-17 13:26:55 +0530

    Update

--- pycosmo/distributions/density_field/genextreem.py
+++ pycosmo/distributions/density_field/genextreem.py
@@ -1,5 +1,6 @@
 from typing import Any 
 from itertools import product, repeat
+from scipy.interpolate import CubicSpline
 from pycosmo.cosmology import Cosmology
 from pycosmo.distributions.base import Distribution, DistributionError
 import pycosmo.utils.numeric as numeric 
@@ -8,7 +9,12 @@ import numpy as np
 
 class GenExtremeDistribution(Distribution):
 
-    __slots__ = 'cosmology', 'z', 'r', 'kn', 'b2_log', 'power_law'
+    # global setting for objects
+    INTERP_N     = 51    # number of interpolation points
+    MEAN_N       = 501   # number of samples for averaging
+    EXACT_GROWTH = False # use exact growth factor 
+
+    __slots__ = 'cosmology', 'z', 'r', 'kn', 'b2_log', 'meas_power_spectrum'
 
     def __init__(self, cm: Cosmology, r: float, z: float = 0) -> None:
         
@@ -22,7 +28,8 @@ class GenExtremeDistribution(Distribution):
         self.z  = z
 
         self.b2_log    = 1.0 # log field bias 
-        self.power_law = None
+
+        self._prepareSpline()
 
     def pdf(self, *args: Any, **kwargs: Any) -> Any:
         ...
@@ -32,7 +39,59 @@ class GenExtremeDistribution(Distribution):
 
     def supportInterval(self) -> tuple:
         ...
-                
+    
+    def measuredPowerSepctrum(self, kx: Any, ky: Any, kz: Any, z: float = 0) -> Any:
+        r"""
+        Return the measured log-field power spectrum.
+        """
+        kx, ky, kz = np.asfarray( kx ), np.asfarray( ky ), np.asfarray( kz )
+        
+        lnk = 0.5*np.log( kx**2 + ky**2 + kz**2 )
+        pk  = np.exp( self.meas_power_spectrum( lnk ) )
+        return pk * self.cosmology.Dplus( z, exact = self.EXACT_GROWTH ) * self.b2_log
+
+    def _measuredPowerSpectrum(self, kx: Any, ky: Any, kz: Any) -> Any:
+
+        def weightedPowerTerm(kx: Any, ky: Any, kz: Any) -> Any:
+            p = 1
+            k  = np.sqrt( kx**2 + ky**2 + kz**2 )
+
+            # log field power spectrum
+            Pk = self.cosmology.linearPowerSpectrum( k, z = 0, dim = True )
+
+            # mass-assignment function
+            Wk = ( np.sinc( 0.5*kx / self.kn ) * np.sinc( 0.5*ky / self.kn ) * np.sinc( 0.5*kz / self.kn ) )**p
+
+            return Pk * Wk**2 
+
+        kx, ky, kz = np.asfarray( kx ), np.asfarray( ky ), np.asfarray( kz )
+
+        y = 0.0
+        for nx, ny, nz in product( *repeat( range(3), 3 ) ):
+
+            # need only n's with length < 3
+            if nx**2 + ny**2 + nz**2 > 9:
+                continue
+
+            y += weightedPowerTerm( kx + 2*nx*self.kn, ky + 2*ny*self.kn, kz + 2*nz*self.kn )
+        
+        return y 
+
+    def _prepareSpline(self) -> None:
+
+        k     = np.logspace( -4, np.log10( self.kn ), self.INTERP_N )
+        theta = np.random.uniform( 0.0,   np.pi, ( self.MEAN_N, self.INTERP_N ) )
+        phi   = np.random.uniform( 0.0, 2*np.pi, ( self.MEAN_N, self.INTERP_N ) )
+
+        Pk = self._measuredPowerSpectrum( 
+                                            kx = k * np.sin( theta ) * np.cos( phi ),
+                                            ky = k * np.sin( theta ) * np.sin( phi ),
+                                            kz = k * np.cos( theta )
+                                        ).mean( axis = 0 ) 
+
+        self.meas_power_spectrum = CubicSpline( np.log(k), np.log(Pk), bc_type = 'natural' )
+        return
+
     def sigma2Linear(self, z: float = 0) -> float:
         r"""
         Linear variance in the box, using a k-space tophat filter (sharp-k filter) :math:`\sigma^2_{\rm lin}( k_N )`.
@@ -76,38 +135,22 @@ class GenExtremeDistribution(Distribution):
         """
         mu = 0.73
         return mu * np.log( 1 + arg / mu )
-    
-    def measuredPowerSepctrum(self, kx: Any, ky: Any, kz: Any) -> Any:
+
+    def sigma2Box(self, z: float = 0) -> float:
         r"""
-        Return the measured log-field power spectrum.
+        Return the measured value of log-field variance, :math:`\sigma^2_A(l)`.
+
+        Parameters
+        ----------
+        z: float, optional
+            Redshift.
+
+        Returns
+        -------
+        var: float
+            Log field variance.
+
         """
-
-    def _measuredPowerSpectrum(self, kx: Any, ky: Any, kz: Any, z: float = 0) -> Any:
-
-        def weightedPowerTerm(kx: Any, ky: Any, kz: Any, z: float) -> Any:
-            k  = np.sqrt( kx**2 + ky**2 + ky**2 )
-
-            # log field power spectrum
-            Pk = self.cosmology.linearPowerSpectrum( k, z, dim = True )
-
-            # mass-assignment function
-            Wk = ( np.sinc( 0.5*kx / self.kn ) * np.sinc( 0.5*ky / self.kn ) * np.sinc( 0.5*kz / self.kn ) )
-
-            return Pk * Wk**2 
-
-        kx, ky, kz = np.asfarray( kx ), np.asfarray( ky ), np.asfarray( kz )
-
-        y = 0.0
-        for nx, ny, nz in product( *repeat( range(3), 3 ) ):
-
-            # need only n's with length < 3
-            if nx**2 + ny**2 + nz**2 > 9:
-                continue
-
-            y += weightedPowerTerm( kx + 2*nx*self.kn, ky + 2*nx*self.kn, kz + 2*nx*self.kn, z )
-        
-        return y * self.b2_log 
-
     
 
 
--- test5.py
+++ test5.py
@@ -16,18 +16,69 @@ def test1():
     kx, ky = np.mgrid[ -a:a:501j, -a:a:501j ]
     kz     = 0
 
+    fig, ( ( ax1, ax2 ), ( ax3, ax4 ) ) = plt.subplots(2, 2, )
+
     y = p._measuredPowerSpectrum( kx, ky, kz )
+    ax1.contourf( kx, ky, y )
 
+    y = p._measuredPowerSpectrum( kx, ky, kz )
+    ax2.contourf( kx, ky, y )
 
-    plt.figure()
+    y = p._measuredPowerSpectrum( kx, ky, kz )
+    ax3.contourf( kx, ky, y )
 
-    plt.contourf( kx, ky, y )
+    ###################################################################
+
+    x = np.logspace( -3, np.log10(p.kn), 201 )
+
+    ax4.loglog()
+
+    y = p._measuredPowerSpectrum( x, 0, 0 )
+    ax4.plot(x, y)
+
+    y = p._measuredPowerSpectrum( 0, x, 0 )
+    ax4.plot(2*x, y)
+
+    y = p._measuredPowerSpectrum( 0, 0, x )
+    ax4.plot(4*x, y)
 
     plt.show()
 
     return
 
 
+def test2():
+    c = Cosmology( 0.7, 0.3, 0.05, 0.8, 1.0, power_spectrum = 'eisenstein98_zb' )
+
+    p = GenExtremeDistribution( c, 1.95, 0  )
+
+    plt.figure()
+    plt.loglog()  
+
+    n1, n2 = 51, 501
+    k = np.logspace( -4, np.log10(p.kn), n1 )
+
+    t = np.random.uniform( 0, np.pi, (n2, n1) )
+    s = np.random.uniform( 0, 2*np.pi, (n2, n1) )
+    
+    kx = k*np.sin(t)*np.cos(s)
+    ky = k*np.sin(t)*np.sin(s)
+    kz = k*np.cos(t)
+
+    y = np.mean( p._measuredPowerSpectrum(kx, ky, kz), axis = 0 )
+
+    plt.plot( k/p.kn, y  )
+
+    k1 = np.sqrt( kx**2 + ky**2 + kz**2 ).flatten()
+    y1 = p.measuredPowerSepctrum( kx, ky, kz ).flatten()
+
+    plt.plot( k1/p.kn, y1, 'o', ms = 3 )
+
+
+    plt.show()
+
+    return
+
 
 if __name__ == '__main__':
-    test1()
+    test2()
